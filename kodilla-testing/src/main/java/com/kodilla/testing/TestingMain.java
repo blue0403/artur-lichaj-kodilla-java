d545dbc665e033353146414737859396f4fcef30
blue0403 <artur.lichaj@gmail.com>
2018-03-14 20:56:03 +0100

    Third Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,34 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("Test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+        int resultOfAdding = calculator.addAToB(25, 24);
+        int resultOfSubtracting = calculator.subtractAFromB(28, 149);
+
+        if (resultOfAdding == 49){
+            System.out.println("Test of adding is OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        if (resultOfSubtracting == 121){
+            System.out.println("Test of subtracting is OK");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
