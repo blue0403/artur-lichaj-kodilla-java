b38457239946ddd0aef848df56deb64be3efe6fc
blue0403 <artur.lichaj@gmail.com>
2018-03-23 23:10:01 +0100

    TextBeautifier

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,4 +1,62 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.*;
+import com.kodilla.stream.reference.BeautificationTypes;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
 public class StreamMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to module 7 - Stream");
+
+        //Task 1
+        System.out.println("\nCreating an example text:");
+        //First way
+        SaySomething saySomething = new SaySomething();
+        saySomething.say();
+
+        //Second way
+        Processor processor = new Processor();
+        ExecuteSaySomething executeSaySomething = new ExecuteSaySomething();
+        processor.execute(executeSaySomething);
+
+        //Third way - Lambda expression
+        //Processor processor = new Processor();
+        Executor codeToExecute = () -> System.out.println("This is an example text.");
+        processor.execute(codeToExecute);
+
+        //Fourth way
+        //Processor processor = new Processor();
+        processor.execute(() -> System.out.println("This is an example text."));
+
+        //Task 2
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        System.out.println("\nCalculating expressions with lambdas");
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        //Task 3
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+
+        System.out.println("\nBeautifying the text with lambdas");
+        poemBeautifier.beautify("First text to beautify", text -> text.toUpperCase());
+        poemBeautifier.beautify("Second text to beautify", text -> "ABC" + text + "ABC");
+        poemBeautifier.beautify("Third text to beautify", text -> new StringBuilder(text).reverse().toString());
+        poemBeautifier.beautify("Fourth text to beautify", text -> text + " (length of the text: " + text.length() + ")");
+
+        System.out.println("Beautifying the text with method references");
+        poemBeautifier.beautify("First text to beautify", BeautificationTypes::createUpperCase);
+        poemBeautifier.beautify("Second text to beautify", BeautificationTypes::addABCAtBeginningAndEndOfText);
+        poemBeautifier.beautify("Third text to beautify", BeautificationTypes::reverseTheOrderOfLetters);
+        poemBeautifier.beautify("Fourth text to beautify", BeautificationTypes::addTextLength);
+    }
 }
--- kodilla-stream/src/main/java/com/kodilla/stream/reference/BeautificationTypes.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/reference/BeautificationTypes.java
@@ -1,4 +1,19 @@
 package com.kodilla.stream.reference;
 
 public class BeautificationTypes {
+    public static String createUpperCase (String text){
+        return text.toUpperCase();
+    }
+
+    public static String addABCAtBeginningAndEndOfText (String text){
+        return "ABC" + text + "ABC";
+    }
+
+    public static String reverseTheOrderOfLetters (String text){
+        return new StringBuilder(text).reverse().toString();
+    }
+
+    public static String addTextLength (String text){
+        return text + " (length of the text: " + text.length() + ")";
+    }
 }
