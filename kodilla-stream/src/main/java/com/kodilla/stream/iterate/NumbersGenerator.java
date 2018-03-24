b7dc7c3872eceb6d0de5c205c1ced039e86a7867
blue0403 <artur.lichaj@gmail.com>
2018-03-24 22:22:16 +0100

    NumbersGenerator - example of using Stream

--- kodilla-stream/src/main/java/com/kodilla/stream/iterate/NumbersGenerator.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/iterate/NumbersGenerator.java
@@ -2,7 +2,7 @@ package com.kodilla.stream.iterate;
 
 import java.util.stream.Stream;
 
-public class NumbersGenerator {
+public final class NumbersGenerator {
     public static void generateEven(int max) {
         Stream.iterate(1, n -> n + 1)
                 .limit(max)
