a8d726f8a70226bcf48c4e509f67aa82eb543db5
blue0403 <artur.lichaj@gmail.com>
2018-03-27 21:17:58 +0200

    Calculating the average value using IntStream - functional programming

--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -1,4 +1,18 @@
 package com.kodilla.stream.array;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ArrayOperationsTestSuite {
+    @Test
+    public void testGetAverage(){
+        //Given
+        int[] randomNumbers = {2, 5, 14, 19, 23, 26, 31, 38, 44, 47, 53, 59, 62, 65, 70, 78, 81, 84, 92, 99};
+
+        //When
+        double averageOfNumbers = ArrayOperations.getAverage(randomNumbers);
+
+        //Then
+        Assert.assertEquals(49.6, averageOfNumbers, 0);
+    }
 }
