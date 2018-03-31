ec6ae3e643828375a463dbadf63f376b3fc4b22e
blue0403 <artur.lichaj@gmail.com>
2018-03-31 15:04:36 +0200

    Add FileReaderWithoutHandling in main() method

--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -1,11 +1,25 @@
 package com.kodilla.exception.main;
 
 import com.kodilla.exception.io.FileReader;
+import com.kodilla.exception.io.FileReaderWithoutHandling;
+
+import java.io.IOException;
 
 public class ExceptionModuleRunner {
     public static void main(String args[]) {
 
+        //First way - FileReader with handling
         FileReader fileReader = new FileReader();
         fileReader.readFile();
+
+        //Second way - FileReader without handling
+        FileReaderWithoutHandling fileReaderWithoutHandling = new FileReaderWithoutHandling();
+
+        try {
+            fileReaderWithoutHandling.readFile();
+
+        } catch (IOException e) {
+            System.out.println("Problem while reading a file! Error " + e);
+        }
     }
 }
