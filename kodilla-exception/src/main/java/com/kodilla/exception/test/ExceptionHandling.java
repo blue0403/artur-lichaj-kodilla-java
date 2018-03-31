e4787c626bd45e785b72932b4c9a86b5a34fc381
blue0403 <artur.lichaj@gmail.com>
2018-03-31 16:43:14 +0200

    Create main() method and add Exception handling

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -1,4 +1,19 @@
 package com.kodilla.exception.test;
 
 public class ExceptionHandling {
+    public static void main(String args[]) {
+
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        try {
+            String result = secondChallenge.probablyIWillThrowException(2, 1);
+            System.out.println(result);
+
+        } catch (Exception e) {
+            System.out.println("Range exceeded! Error " + e);
+
+        } finally {
+            System.out.println("x should belong to the range <1, 2), and y be different from 1.5!");
+        }
+    }
 }
