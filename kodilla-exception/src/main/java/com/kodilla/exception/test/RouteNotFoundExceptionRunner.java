11b148525216c68202cf74cd3559040e5b88709b
blue0403 <artur.lichaj@gmail.com>
2018-04-04 15:27:16 +0200

    Add RouteNotFoundException handling

--- kodilla-exception/src/main/java/com/kodilla/exception/test/RouteNotFoundExceptionRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/RouteNotFoundExceptionRunner.java
@@ -4,5 +4,14 @@ public class RouteNotFoundExceptionRunner {
 
     public static void main(String[] args) {
 
+        Flight WAW_MOS = new Flight("Warsaw", "Moscow");
+        FlightSearchEngine flightSearchEngine = new FlightSearchEngine();
+
+        try {
+            flightSearchEngine.findFlight(WAW_MOS);
+
+        } catch (RouteNotFoundException e) {
+            System.out.println("We are so sorry, flight to this airport is not possible");
+        }
     }
 }
