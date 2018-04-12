2b449ee980b1a04332e47697753bb39805a3512e
blue0403 <artur.lichaj@gmail.com>
2018-04-12 21:05:53 +0200

    Create class OrderProcessor and add method process()

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/OrderProcessor.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/OrderProcessor.java
@@ -1,4 +1,25 @@
 package com.kodilla.good.patterns.challenges;
 
 public class OrderProcessor {
+    private InformationService informationService;
+    private OrderService orderService;
+    private OrderRepository orderRepository;
+
+    public OrderProcessor(InformationService informationService, OrderService orderService, OrderRepository orderRepository) {
+        this.informationService = informationService;
+        this.orderService = orderService;
+        this.orderRepository = orderRepository;
+    }
+
+    public OrderDTO process(final OrderRequest orderRequest) {
+        boolean isOrdered = orderService.order(orderRequest.getUser(), orderRequest.getOrderedProducts(), orderRequest.getOrderTime());
+
+        if (isOrdered) {
+            informationService.inform(orderRequest.getUser(), orderRequest.getOrderedProducts(), orderRequest.getOrderTime());
+            orderRepository.createOrder(orderRequest.getUser(), orderRequest.getOrderedProducts(), orderRequest.getOrderTime());
+            return new OrderDTO(orderRequest.getUser(), true);
+        } else {
+            return new OrderDTO(orderRequest.getUser(), false);
+        }
+    }
 }
