fc5ee05f9d7d5aa831e89e5d77b17a138c8dbb52
blue0403 <artur.lichaj@gmail.com>
2018-04-12 20:58:06 +0200

    Add implementation of method inform()

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MailService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MailService.java
@@ -1,4 +1,16 @@
 package com.kodilla.good.patterns.challenges;
 
-public class MailService {
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class MailService implements InformationService {
+
+    @Override
+    public void inform(final User user, final List<Product> orderedProducts, final LocalDateTime orderTime) {
+        System.out.println("Welcome " + user.getUsername() + ",\n\nYou have ordered in our service:" + "\n");
+        for(Product product : orderedProducts) {
+            System.out.println(product);
+        }
+        System.out.println("\nDate of order: " + orderTime);
+    }
 }
