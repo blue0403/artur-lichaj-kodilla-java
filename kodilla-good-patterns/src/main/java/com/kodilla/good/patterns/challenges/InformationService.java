cc6714f1633571a4a4c281d01c7ec80e07c4ab17
blue0403 <artur.lichaj@gmail.com>
2018-04-12 20:48:31 +0200

    Add method inform()

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/InformationService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/InformationService.java
@@ -1,4 +1,8 @@
 package com.kodilla.good.patterns.challenges;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface InformationService {
+    void inform(final User user, final List<Product> productList, final LocalDateTime orderTime);
 }
