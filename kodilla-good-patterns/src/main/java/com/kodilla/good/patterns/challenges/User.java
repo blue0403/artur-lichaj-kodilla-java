427b8fed47e2abf05876ffb68859d0ebf804cdee
blue0403 <artur.lichaj@gmail.com>
2018-04-12 20:35:01 +0200

    Create class User

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/User.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/User.java
@@ -1,4 +1,40 @@
 package com.kodilla.good.patterns.challenges;
 
 public class User {
+    private String username;
+    private String realName;
+    private String surname;
+
+    public User(final String username, final String realName, final String surname) {
+        this.username = username;
+        this.realName = realName;
+        this.surname = surname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+
+        User user = (User) o;
+
+        return username.equals(user.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return username.hashCode();
+    }
 }
