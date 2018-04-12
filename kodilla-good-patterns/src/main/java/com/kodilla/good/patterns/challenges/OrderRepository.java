74c81e2697f798536fc06addd22df89fe05ca618
blue0403 <artur.lichaj@gmail.com>
2018-04-12 20:50:06 +0200

    Add method createOrder()

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/OrderRepository.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/OrderRepository.java
@@ -1,4 +1,8 @@
 package com.kodilla.good.patterns.challenges;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface OrderRepository {
+    boolean createOrder(final User user, final List<Product> productsList, final LocalDateTime orderTime);
 }
