c3ba58c08baf26975834be66716bcaf6ad1750c9
blue0403 <artur.lichaj@gmail.com>
2018-04-12 20:59:30 +0200

    Add implementation of method createOrder()

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/ProductOrderRepository.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/ProductOrderRepository.java
@@ -1,4 +1,23 @@
 package com.kodilla.good.patterns.challenges;
 
-public class ProductOrderRepository {
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductOrderRepository implements OrderRepository {
+
+    @Override
+    public boolean createOrder(final User user, final List<Product> productsList, final LocalDateTime orderTime) {
+        boolean result = false;
+
+        OrderRequest order = new OrderRequest(user, productsList, orderTime);
+        List<OrderRequest> orderList = new ArrayList<>();
+        orderList.add(order);
+
+        if (orderList.contains(order)) {
+            result = true;
+        }
+
+        return result;
+    }
 }
