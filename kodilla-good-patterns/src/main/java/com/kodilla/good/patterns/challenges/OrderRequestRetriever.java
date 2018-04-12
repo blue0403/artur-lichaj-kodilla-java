785e4ecc9c09032af0bbd259b1d7298ab07d9253
blue0403 <artur.lichaj@gmail.com>
2018-04-12 20:46:04 +0200

    Add method retrieve() with data

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/OrderRequestRetriever.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/OrderRequestRetriever.java
@@ -1,4 +1,23 @@
 package com.kodilla.good.patterns.challenges;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class OrderRequestRetriever {
+
+    public OrderRequest retrieve() {
+
+        User user = new User("red21", "Matthew", "Redstone");
+
+        List<Product> orderedProducts = new ArrayList<>();
+        orderedProducts.add(new Book());
+        orderedProducts.add(new Game());
+        orderedProducts.add(new Slippers());
+        orderedProducts.add(new Toothbrush());
+
+        LocalDateTime orderTime = LocalDateTime.of(2018, 4, 12, 5, 39);
+
+        return new OrderRequest(user, orderedProducts, orderTime);
+    }
 }
