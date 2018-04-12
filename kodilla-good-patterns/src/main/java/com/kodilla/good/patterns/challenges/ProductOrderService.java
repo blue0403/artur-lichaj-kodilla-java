2ccd706f93bd3c8067ea3e5b6e141929c6906d4a
blue0403 <artur.lichaj@gmail.com>
2018-04-12 21:00:37 +0200

    Add implementation of method order()

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/ProductOrderService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/ProductOrderService.java
@@ -1,4 +1,18 @@
 package com.kodilla.good.patterns.challenges;
 
-public class ProductOrderService {
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class ProductOrderService implements OrderService {
+
+    @Override
+    public boolean order(final User user, final List<Product> productList, final LocalDateTime orderTime) {
+        boolean result = false;
+
+        if (user !=  null && productList.size() > 0 && orderTime != null) {
+            result = true;
+        }
+
+        return result;
+    }
 }
