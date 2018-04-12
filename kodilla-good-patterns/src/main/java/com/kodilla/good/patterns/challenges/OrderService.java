2a3ed5b2db15e6d904376408d02fb5add357964c
blue0403 <artur.lichaj@gmail.com>
2018-04-12 20:51:23 +0200

    Add method order()

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/OrderService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/OrderService.java
@@ -1,4 +1,8 @@
 package com.kodilla.good.patterns.challenges;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface OrderService {
+    boolean order(final User user, final List<Product> productList, final LocalDateTime orderTime);
 }
