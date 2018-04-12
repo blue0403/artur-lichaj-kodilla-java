27aa3dd14da7edd4738d2c56df3f87f3f31eeec0
blue0403 <artur.lichaj@gmail.com>
2018-04-12 20:43:26 +0200

    Create class OrderRequest

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/OrderRequest.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/OrderRequest.java
@@ -1,4 +1,28 @@
 package com.kodilla.good.patterns.challenges;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class OrderRequest {
+    private User user;
+    private List<Product> orderedProducts;
+    private LocalDateTime orderTime;
+
+    public OrderRequest(final User user, final List<Product> orderedProducts, final LocalDateTime orderTime) {
+        this.user = user;
+        this.orderTime = orderTime;
+        this.orderedProducts = orderedProducts;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public List<Product> getOrderedProducts() {
+        return orderedProducts;
+    }
+
+    public LocalDateTime getOrderTime() {
+        return orderTime;
+    }
 }
