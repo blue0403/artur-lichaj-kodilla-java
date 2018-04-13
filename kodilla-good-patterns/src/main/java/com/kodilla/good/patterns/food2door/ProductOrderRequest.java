ddb35835efa0dbd36827d526cb730f46056b19b2
blue0403 <artur.lichaj@gmail.com>
2018-04-13 22:09:10 +0200

    Create class ProductOrderRequest

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/food2door/ProductOrderRequest.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/food2door/ProductOrderRequest.java
@@ -1,4 +1,25 @@
 package com.kodilla.good.patterns.food2door;
 
 public class ProductOrderRequest {
+    private String supplier;
+    private double quantityInKG;
+    private String product;
+
+    public ProductOrderRequest(final String supplier, final double quantityInKG, final String product) {
+        this.supplier = supplier;
+        this.quantityInKG = quantityInKG;
+        this.product = product;
+    }
+
+    public String getSupplier() {
+        return supplier;
+    }
+
+    public double getQuantityInKG() {
+        return quantityInKG;
+    }
+
+    public String getProduct() {
+        return product;
+    }
 }
