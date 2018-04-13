ab29918c797f7b70a6390372ecb6a7fd99f79d11
blue0403 <artur.lichaj@gmail.com>
2018-04-13 22:14:28 +0200

    Add method main() to check working of application

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/food2door/Application.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/food2door/Application.java
@@ -1,4 +1,12 @@
 package com.kodilla.good.patterns.food2door;
 
 public class Application {
+    public static void main(String[] args) {
+
+        ProductOrderRequestRetriever retriever = new ProductOrderRequestRetriever();
+        ProductOrderRequest productOrderRequest = retriever.retrieve();
+
+        OrderValidator orderValidator = new OrderValidator(new HealthyShop());
+        orderValidator.checkOrder(productOrderRequest);
+    }
 }
