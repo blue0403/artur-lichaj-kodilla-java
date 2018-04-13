411be1cac7ee5b1f95fc6d72ba50ec5b62a89742
blue0403 <artur.lichaj@gmail.com>
2018-04-13 22:13:02 +0200

    Add example data

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/food2door/ProductOrderRequestRetriever.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/food2door/ProductOrderRequestRetriever.java
@@ -1,4 +1,13 @@
 package com.kodilla.good.patterns.food2door;
 
 public class ProductOrderRequestRetriever {
+
+    public ProductOrderRequest retrieve() {
+
+        String supplier = "ExtraFoodShop";
+        Double quantityInKG = 34.8;
+        String product = "banana";
+
+        return new ProductOrderRequest(supplier, quantityInKG, product);
+    }
 }
