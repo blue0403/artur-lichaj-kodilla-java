7685ae7e572e0285d177e06647405ae8215f2761
blue0403 <artur.lichaj@gmail.com>
2018-04-16 14:27:34 +0200

    Add classes that inherit from class Figure

--- kodilla-spring-intro/src/main/java/com/kodilla/spring/intro/shape/Circle.java
+++ kodilla-spring-intro/src/main/java/com/kodilla/spring/intro/shape/Circle.java
@@ -1,4 +1,8 @@
 package com.kodilla.spring.intro.shape;
 
-public class Circle {
+public class Circle extends Figure {
+    @Override
+    public void draw() {
+        System.out.println("This is a circle");
+    }
 }
