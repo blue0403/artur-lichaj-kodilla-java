2dd1fcec2bcbe5b010a37fc42d8f2f2e4cd06779
blue0403 <artur.lichaj@gmail.com>
2018-04-23 17:25:10 +0200

    Create Bean of class ForumUser

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -0,0 +1,12 @@
+package com.kodilla.spring.forum;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ForumUser {
+    private String username;
+
+    public String getUsername() {
+        return username = "John Smith";
+    }
+}
