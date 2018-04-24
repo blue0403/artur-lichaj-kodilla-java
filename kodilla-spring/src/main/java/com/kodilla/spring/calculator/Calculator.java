249c971beda710451d0dca5219deec771cc7fcb7
blue0403 <artur.lichaj@gmail.com>
2018-04-24 20:06:40 +0200

    Create calculator with 4 methods (add, sub, mul, div)

--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
@@ -1,4 +1,35 @@
 package com.kodilla.spring.calculator;
 
-public class Calculator {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public final class Calculator {
+
+    @Autowired
+    private Display display;
+
+    public double add(double a, double b) {
+        double result = a + b;
+        System.out.println("Result of adding " + a + " and " + b + ":");
+        return display.displayValue(result);
+    }
+
+    public double sub(double a, double b) {
+        double result = a - b;
+        System.out.println("Result of subtracting " + b + " from " + a + ":");
+        return display.displayValue(result);
+    }
+
+    public double mul(double a, double b) {
+        double result = a * b;
+        System.out.println("Result of multiplying " + a + " and " + b + ":");
+        return display.displayValue(result);
+    }
+
+    public double div(double a, double b) {
+        double result = a / b;
+        System.out.println("Result of dividing " + a + " by " + b + ":");
+        return display.displayValue(result);
+    }
 }
