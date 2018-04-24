cb63792ec06b4dfbb5e352960b35be77c6eb38ad
blue0403 <artur.lichaj@gmail.com>
2018-04-24 14:40:52 +0200

    Replace getBean(Class class) with getBean(String name)

--- kodilla-spring/src/test/java/com/kodilla/spring/SpringRunnerTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/SpringRunnerTestSuite.java
@@ -1,9 +1,6 @@
 package com.kodilla.spring;
 
-import com.kodilla.spring.shape.Circle;
 import com.kodilla.spring.shape.Shape;
-import com.kodilla.spring.shape.Square;
-import com.kodilla.spring.shape.Triangle;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,7 +18,7 @@ public class SpringRunnerTestSuite {
 		//Given
 		ApplicationContext context =
 				new AnnotationConfigApplicationContext("com.kodilla.spring");
-		Shape shape = context.getBean(Circle.class);
+		Shape shape = (Shape)context.getBean("circle");
 		//When
 		String name = shape.getShapeName();
 		//Then
@@ -33,7 +30,7 @@ public class SpringRunnerTestSuite {
 		//Given
 		ApplicationContext context =
 				new AnnotationConfigApplicationContext("com.kodilla.spring");
-		Shape shape = context.getBean(Triangle.class);
+		Shape shape = (Shape)context.getBean("triangle");
 		//When
 		String name = shape.getShapeName();
 		//Then
@@ -45,7 +42,7 @@ public class SpringRunnerTestSuite {
 		//Given
 		ApplicationContext context =
 				new AnnotationConfigApplicationContext("com.kodilla.spring");
-		Shape shape = context.getBean(Square.class);
+		Shape shape = (Shape)context.getBean("createSquare");
 		//When
 		String name = shape.getShapeName();
 		//Then
@@ -64,4 +61,3 @@ public class SpringRunnerTestSuite {
         System.out.println("Choosen shape says: " + name);
     }
 }
-
