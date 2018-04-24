ad4c520f7bb41041d44c103edd13e4ca2b57df20
blue0403 <artur.lichaj@gmail.com>
2018-04-24 20:08:00 +0200

    Add testCalculationsOfAdding()

--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -1,4 +1,28 @@
 package com.kodilla.spring.calculator;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class CalculatorTestSuite {
+
+    @Test
+    public void testCalculationsOfAdding() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring.calculator");
+        Calculator calculator = context.getBean(Calculator.class);
+
+        //When
+        double result = calculator.add(2.67, 2.33);
+
+        //Then
+        Assert.assertEquals(5.0, result, 0.01);
+    }
 }
