8bbb467c38ba2d1aa5ba46ccc65577224588fa06
blue0403 <artur.lichaj@gmail.com>
2018-04-25 13:32:38 +0200

    Add testRead()

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/reader/ReaderTestSuite.java
@@ -0,0 +1,19 @@
+package com.kodilla.spring.reader;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootTest
+public class ReaderTestSuite {
+
+    @Test
+    public void testRead() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext(ReaderConfig.class);
+        Reader reader = context.getBean(Reader.class);
+        //When & Then
+        reader.read();
+    }
+}
