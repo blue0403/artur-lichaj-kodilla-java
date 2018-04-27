a398fac6666d80e204c43ea11ce4dcd5cd207473
blue0403 <artur.lichaj@gmail.com>
2018-04-27 21:51:08 +0200

    Create a User class and classes that inherit from it

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/strategy/social/User.java
@@ -0,0 +1,13 @@
+package com.kodilla.patterns.strategy.social;
+
+public class User {
+    private final String username;
+
+    public User(final String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
