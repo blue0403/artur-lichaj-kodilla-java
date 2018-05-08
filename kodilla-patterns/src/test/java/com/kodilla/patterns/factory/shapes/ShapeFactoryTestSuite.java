6f511015d0f4644a95e88457d92ca646c1538712
blue0403 <artur.lichaj@gmail.com>
2018-05-08 14:31:43 +0200

    Add testFactoryCircle()

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/factory/shapes/ShapeFactoryTestSuite.java
@@ -0,0 +1,18 @@
+package com.kodilla.patterns.factory.shapes;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ShapeFactoryTestSuite {
+
+    @Test
+    public void testFactoryCircle() {
+        //Given
+        ShapeFactory factory = new ShapeFactory();
+        //When
+        Shape circle = factory.makeShape(ShapeFactory.CIRCLE);
+        //Then
+        Assert.assertEquals(Math.PI * Math.pow(4.50, 2.0), circle.getArea(), 0);
+        Assert.assertEquals("The rounded circle", circle.getName());
+    }
+}
