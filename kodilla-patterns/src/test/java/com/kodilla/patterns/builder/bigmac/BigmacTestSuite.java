4e5d9aacf5f4c558256d5c7b1bad4e1208611753
blue0403 <artur.lichaj@gmail.com>
2018-05-09 18:23:30 +0200

    Add test BigmacNew()

--- kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -1,4 +1,33 @@
 package com.kodilla.patterns.builder.bigmac;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class BigmacTestSuite {
+
+    @Test
+    public void testBigmacNew() {
+        //Given
+        Bigmac bigmac = new Bigmac.BigmacBuilder()
+                .ingredient("Lettuce")
+                .ingredient("Onion")
+                .ingredient("Bacon")
+                .bun("With sesame seeds")
+                .sauce("Barbecue")
+                .burgers(3)
+                .ingredient("Cucumber")
+                .ingredient("Chili Peppers")
+                .ingredient("Mushrooms")
+                .ingredient("Shrimps")
+                .ingredient("Cheese")
+                .build();
+        System.out.println(bigmac);
+        //When
+        int howManyIngredients = bigmac.getIngredients().size();
+        //Then
+        Assert.assertEquals(8, howManyIngredients);
+        Assert.assertEquals(3, bigmac.getBurgers());
+        Assert.assertEquals("With sesame seeds", bigmac.getBun());
+        Assert.assertEquals("Barbecue", bigmac.getSauce());
+    }
 }
