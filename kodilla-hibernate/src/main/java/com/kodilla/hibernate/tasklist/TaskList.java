03681f35a573e59a64f42276c8cc9b789ae03816
blue0403 <artur.lichaj@gmail.com>
2018-05-17 17:27:39 +0200

    Prepare the class for use by Hibernate

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
@@ -2,12 +2,19 @@ package com.kodilla.hibernate.tasklist;
 
 import com.kodilla.hibernate.task.Task;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
+@Entity
+@Table(name = "TASKLISTS")
 public final class TaskList {
     private int id;
     private List<Task> listName;
     private String description;
+
+    public TaskList() {
+    }
 
     public TaskList(int id, List<Task> listName, String description) {
         this.id = id;
@@ -15,15 +22,34 @@ public final class TaskList {
         this.description = description;
     }
 
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "ID", unique = true)
     public int getId() {
         return id;
     }
 
+    @NotNull
+    @Column(name = "LISTNAME")
     public List<Task> getListName() {
         return listName;
     }
 
+    @Column(name = "DESCRIPTION")
     public String getDescription() {
         return description;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    private void setListName(List<Task> listName) {
+        this.listName = listName;
+    }
+
+    private void setDescription(String description) {
+        this.description = description;
     }
 }
