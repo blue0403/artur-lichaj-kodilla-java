b3ba30a54352fdcbe8a4137834f7d231dfd5c785
blue0403 <artur.lichaj@gmail.com>
2018-05-18 18:29:57 +0200

    Add a N:M relationship between COMPANIES and EMPLOYEES entities

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Company.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/manytomany/Company.java
@@ -2,12 +2,15 @@ package com.kodilla.hibernate.manytomany;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "COMPANIES")
 public class Company {
     private int id;
     private String name;
+    private List<Employee> employees = new ArrayList<>();
 
     public Company() {
     }
@@ -30,11 +33,20 @@ public class Company {
         return name;
     }
 
+    @ManyToMany(cascade = CascadeType.ALL, mappedBy = "companies")
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
     private void setId(int id) {
         this.id = id;
     }
 
     private void setName(String name) {
         this.name = name;
+    }
+
+    private void setEmployees(List<Employee> employees) {
+        this.employees = employees;
     }
 }
