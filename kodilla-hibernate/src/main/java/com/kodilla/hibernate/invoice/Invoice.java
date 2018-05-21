89599d66db8ae1cdd7cba9fc49ace1373c19a486
blue0403 <artur.lichaj@gmail.com>
2018-05-21 21:47:06 +0200

    Add a 1:N relationship between INVOICES and ITEMS entities

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/invoice/Invoice.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/invoice/Invoice.java
@@ -2,12 +2,15 @@ package com.kodilla.hibernate.invoice;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "INVOICES")
 public final class Invoice {
     private int id;
     private String number;
+    private List<Item> items = new ArrayList<>();
 
     public Invoice() {
     }
@@ -30,11 +33,25 @@ public final class Invoice {
         return number;
     }
 
+    @OneToMany(
+            targetEntity = Item.class,
+            mappedBy = "invoice",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    public List<Item> getItems() {
+        return items;
+    }
+
     private void setId(int id) {
         this.id = id;
     }
 
     private void setNumber(String number) {
         this.number = number;
+    }
+
+    private void setItems(List<Item> items) {
+        this.items = items;
     }
 }
