ec7e6dfd3ea1ebb707b12fcd7c23e85870425b38
blue0403 <artur.lichaj@gmail.com>
2018-07-13 17:44:13 +0200

    Delete unnecessary argument (String name) from the constructor

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/observer/forum/JavaToolsForumTopic.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/observer/forum/JavaToolsForumTopic.java
@@ -1,7 +1,7 @@
 package com.kodilla.patterns2.observer.forum;
 
 public class JavaToolsForumTopic extends ForumTopic {
-    public JavaToolsForumTopic(String name) {
+    public JavaToolsForumTopic() {
         super("Java Tools Group");
     }
 }
