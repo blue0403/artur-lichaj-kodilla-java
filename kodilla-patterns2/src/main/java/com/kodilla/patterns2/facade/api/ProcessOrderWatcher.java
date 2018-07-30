cf37692f627ca955c750684a1833ad3694ecfa1b
blue0403 <artur.lichaj@gmail.com>
2018-07-30 16:36:33 +0200

    Add aspects that write to logs each call to the processOrder() method

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/ProcessOrderWatcher.java
@@ -0,0 +1,19 @@
+package com.kodilla.patterns2.facade.api;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ProcessOrderWatcher {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProcessOrderWatcher.class);
+
+    @Before("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))" +
+    " && args(userId) && target(object)")
+    public void logEvent(OrderDto order, Long userId, Object object) {
+        LOGGER.info("Class: " + object.getClass().getName() + ", Order of user: " + userId);
+    }
+}
