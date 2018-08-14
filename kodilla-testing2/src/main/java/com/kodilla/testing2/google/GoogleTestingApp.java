e6bac947ffee2cbd13de1ed76b3c27da6016e89f
blue0403 <artur.lichaj@gmail.com>
2018-08-14 17:04:15 +0200

    Start Google search engine using the Mozilla Firefox browser

--- kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
@@ -1,8 +1,11 @@
 package com.kodilla.testing2.google;
 
+import com.kodilla.testing2.config.WebDriverConfig;
+import org.openqa.selenium.WebDriver;
+
 public class GoogleTestingApp {
     public static void main(String[] args) {
-        System.setProperty("Webdriver.gecko.driver", "c:\\Selenium-drivers\\Firefox\\geckodriver.exe");
-        System.setProperty("Webdriver.chrome.driver", "c:\\Selenium-drivers\\Chrome\\chromedriver.exe");
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.FIREFOX);
+        driver.get("https://www.google.com");
     }
 }
