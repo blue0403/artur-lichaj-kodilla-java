a0239f265f79392b06084c6fb44ceed7fee818e6
blue0403 <artur.lichaj@gmail.com>
2018-08-14 15:00:36 +0200

    Add the location of drivers to individual browsers

---
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
@@ -0,0 +1,8 @@
+package com.kodilla.testing2.google;
+
+public class GoogleTestingApp {
+    public static void main(String[] args) {
+        System.setProperty("Webdriver.gecko.driver", "c:\\Selenium-drivers\\Firefox\\geckodriver.exe");
+        System.setProperty("Webdriver.chrome.driver", "c:\\Selenium-drivers\\Chrome\\chromedriver.exe");
+    }
+}
