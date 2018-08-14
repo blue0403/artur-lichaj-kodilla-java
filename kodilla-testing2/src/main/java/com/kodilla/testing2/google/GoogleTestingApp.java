7441821f38bf236e5ee56d69ff1794110f40c819
blue0403 <artur.lichaj@gmail.com>
2018-08-14 17:41:51 +0200

    Start Google search engine using the Google Chrome browser

--- kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 
 public class GoogleTestingApp {
     public static void main(String[] args) {
-        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.FIREFOX);
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
         driver.get("https://www.google.com");
     }
 }
